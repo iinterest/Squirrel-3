fbee7d71466a939a3118d25aa50073df709efb3a
iinterest <iinterest.bell@gmail.com>
2013-12-31 13:46:14 +0800

    文档调整

--- docs/static/dpl-famework.js
+++ docs/static/dpl-famework.js
@@ -101,7 +101,100 @@ $(document).ready(function () {
                 }
             });
         }
-        
+    }());
+    
+    (function costomize() {
+        if ($("#customize").length === 0) {
+            return;
+        }
+
+        (function selectEvent () {
+            var $selectAll = $(".J_selectAll");
+            var $checkbox = $("input:checkbox");
+            var $customizeBtn = $(".J_customizeBtn");
+
+            $selectAll.on("click", function () {
+                var $me = $(this);
+                var groupName = $me.attr("data-group");
+                $("input[name="+ groupName +"]").prop("checked", this.checked);
+                // 取消全选的同时，取消所有 DPL 勾选（为了简化逻辑）
+                if (!this.checked) {
+                    $("input[name=dpl]").each(function () {
+                        this.checked = false;
+                    });
+                }
+                updateSelectCountUi();
+            });
+
+            $checkbox.on("click", function () {
+                var $me = $(this);
+                var name = $me.attr("class");
+                var groupName = $me.attr("name");
+                var isAllSelect = true;
+                var i;
+                var len;
+                var components = "";
+                var hash = "#!components={components}";
+
+                // 添加、解除全选勾选
+                $("input[name="+ groupName +"]").each(function () {
+                    if (!this.checked) {
+                        isAllSelect = false;
+                    }
+                });
+                $("input[data-group = "+ groupName +"]").prop("checked", isAllSelect);
+                
+                if (this.attributes["data-required"]) {
+                    // 自动勾选 DPL 所需插件或组件
+                    var requireds = this.attributes["data-required"].value.split(",");
+                    len = requireds.length;
+                    for (i = 0; i < len; i++) {
+                        $("." + requireds[i]).prop("checked", true);
+                    }
+                } else {
+                    // 自动取消当前选项所支持的 DPL 组件
+                    $("input[data-required]").each(function () {
+                        var requireds = this.attributes["data-required"].value.split(",");
+                        if (requireds.indexOf(name) !== -1) {
+                            this.checked = false;
+                        }
+                    });
+                }
+
+                // 计算请求位码
+                $checkbox.each(function () {
+                    if (this.className.indexOf("J_selectAll") !== -1) {
+                        return;
+                    }
+                    if (this.checked) {
+                        components += "1";
+                    } else {
+                        components += "0";
+                    }
+                });
+                hash = hash.replace("{components}", components);
+
+                $customizeBtn.attr("href", hash);
+                updateSelectCountUi();
+            });
+
+        }());
+
+        function updateSelectCountUi () {
+            var groupName = ["components", "plugins", "dpl"];
+            var i;
+            var len = groupName.length;
+            for (i = 0; i < len; i++) {
+                var count = $("input[name="+ groupName[i] +"]:checked").length;
+                var $count = $("." + groupName[i] + "-num");
+                $count.text(count);
+                if (count > 0) {
+                    $count.addClass("hightlight");
+                } else {
+                    $count.removeClass("hightlight");
+                }
+            }
+        }
     }());
 
 });//jQuery ready end
