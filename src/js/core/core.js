b4eac8090a321fefe2f55bd91dae47ec3d800bd1
iinterest <iinterest.bell@gmail.com>
2014-07-08 11:36:47 +0800

    新增函数节流 debounce()、throttle()

--- src/js/core/core.js
+++ src/js/core/core.js
@@ -2,6 +2,7 @@
  * @file SQ.core
  * @version 1.0.0
  */
+/*global SQ*/
 var SQ = {
     /**
      * 命名空间方法
@@ -77,6 +78,11 @@ var SQ = {
      .replace(rvalidbraces, '')) :
      false;
      }*/
+    /**
+     * 继承
+     * @param Child
+     * @param Parent
+     */
     extend: function (Child, Parent) {
         var F = function () {
         };
@@ -84,5 +90,56 @@ var SQ = {
         Child.prototype = new F();
         Child.prototype.constructor = Child;
         Child.uber = Parent.prototype;
+    },
+    /**
+     * 频率控制 返回函数连续调用时，fn 执行频率限定为每多少时间执行一次
+     * @param fn {function}     需要调用的函数
+     * @param delay {number}    延迟时间，单位毫秒
+     * @param immediate {bool}  给 immediate 参数传递 false 绑定的函数先执行，而不是 delay 后后执行。
+     * @return {function}       实际调用函数
+     */
+    throttle: function (fn, delay, immediate, debounce) {
+        var curr;
+        var last_call = 0;
+        var last_exec = 0;
+        var timer = null;
+        var diff;               // 时间差
+        var context;            // 上下文
+        var args;
+        var exec = function () {
+            last_exec = curr;
+            fn.apply(context, args);
+        };
+        return function () {
+            curr = + new Date();
+            context = this;
+            args = arguments;
+            diff = curr - (debounce ? last_call : last_exec) - delay;
+            clearTimeout(timer);
+            if (debounce) {
+                if (immediate) {
+                    timer = setTimeout(exec, delay);
+                } else if (diff >= 0) {
+                    exec();
+                }
+            } else {
+                if (diff >= 0) {
+                    exec();
+                } else if (immediate) {
+                    timer = setTimeout(exec, -diff);
+                }
+            }
+            last_call = curr;
+        };
+    },
+    /**
+     * 空闲控制 返回函数连续调用时，空闲时间必须大于或等于 delay，fn 才会执行
+     * @param fn {function}     要调用的函数
+     * @param delay {number}    空闲时间
+     * @param immediate {bool}  给 immediate 参数传递 false 绑定的函数先执行，而不是 delay 后后执行。
+     * @return {function}       实际调用函数
+     */
+    debounce: function (fn, delay, immediate) {
+        return SQ.throttle(fn, delay, immediate, true);
     }
 };
